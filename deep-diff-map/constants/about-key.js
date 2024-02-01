e196ee860b206be800d807519651b9d7f69eff6e
Serega <sergeykyz19998@gmail.com>
2024-02-01 22:11:26 +0200

    constant encapsulation

---
+++ deep-diff-map/constants/about-key.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const DATA = "_about";
+const CHANGED = "_changed";
+const OLD_VALUES = "_old";
+const OUTER_KEY = "_outerKey";
+const CHANGED_CHILDREN = "_innerChanged";
+const UNDEFINED_CHILDREN_STATUS = "_childrenStatus";
+
+
+module.exports = {
+  DATA,
+  CHANGED,
+  OLD_VALUES,
+  OUTER_KEY,
+  CHANGED_CHILDREN,
+  UNDEFINED_CHILDREN_STATUS,
+};
---
+++ deep-diff-map/constants/status.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const CREATED_STATUS = "created";
+const DELETED_STATUS = "deleted";
+const CHANGED_STATUS = "changed";
+const UNCHANGED_STATUS = "unchanged";
+const DEEP_VALUE_STATUS = "deep_value";
+const ALL_STATUS_NAME = {
+  CREATED_STATUS,
+  DELETED_STATUS,
+  CHANGED_STATUS,
+  UNCHANGED_STATUS,
+  DEEP_VALUE_STATUS,
+};
+
+module.exports = {
+  CREATED_STATUS,
+  DELETED_STATUS,
+  CHANGED_STATUS,
+  UNCHANGED_STATUS,
+  DEEP_VALUE_STATUS,
+  ALL_STATUS_NAME,
+};
